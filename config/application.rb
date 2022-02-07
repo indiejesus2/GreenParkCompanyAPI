ba7ed55b3816834499373f4095c312f95692217f
indiejesus2 <62215604+indiejesus2@users.noreply.github.com>
2022-02-06 20:11:57 -0500

    WIP: Updated Rails application."

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,23 +10,13 @@ module GreenParkCompany
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
 
-    config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
-    config.react.server_renderer_timeout    ||= 20 # seconds
-    config.react.server_renderer = React::ServerRendering::BundleRenderer
-    config.react.server_renderer_options = {
-      files: ["server_rendering.js"],       # files to load for prerendering
-      replay_console: true,                 # if true, console.* will be replayed client-side
-    }
-    # Changing files matching these dirs/exts will cause the server renderer to reload:
-    config.react.server_renderer_extensions = ["jsx", "js"]
-    config.react.server_renderer_directories = ["/app/assets/javascripts", "/app/javascript/"]
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
