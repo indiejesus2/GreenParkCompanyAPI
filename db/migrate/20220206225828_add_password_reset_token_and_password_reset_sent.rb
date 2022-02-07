2a082742a8b5b75742c530b95b4aafb1c162d0ec
indiejesus2 <62215604+indiejesus2@users.noreply.github.com>
2022-02-07 00:05:43 -0500

    WIP: Reverted dependencies

--- db/migrate/20220206225828_add_password_reset_token_and_password_reset_sent.rb
+++ db/migrate/20220206225828_add_password_reset_token_and_password_reset_sent.rb
@@ -1,4 +1,4 @@
-class AddPasswordResetTokenAndPasswordResetSent < ActiveRecord::Migration[7.0]
+class AddPasswordResetTokenAndPasswordResetSent < ActiveRecord::Migration[6.0]
   def change
     add_column :employees, :password_reset_token, :string
     add_column :employees, :password_reset_sent, :datetime
