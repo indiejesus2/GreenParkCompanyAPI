92e25a557b44fb3ce3daac57568cdd26baee3d45
indiejesus2 <62215604+indiejesus2@users.noreply.github.com>
2022-07-11 08:50:01 -0400

    WIP: Forgot some useEffect additions.

--- app/javascript/components/Contractors/EditJob.jsx
+++ app/javascript/components/Contractors/EditJob.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, FloatingLabel, Row, Col, InputGroup } from 'react-bootstrap'
 import { useFormik } from 'formik'
--- app/javascript/components/Questionnaire/Experience.jsx
+++ app/javascript/components/Questionnaire/Experience.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, FloatingLabel, Button, Row, Col } from 'react-bootstrap'
 
 const Experience = props => {
